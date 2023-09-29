9d5eeb264480a8afbe903ada3fb454396ad0ae94
Paulo Henrique <paulo.13.henrique800@gmail.com>
2023-09-29 16:57:34 -0300

    criação da entidade Order(pedido)

---
+++ src/main/java/com/treino/registrodevendascomspringboot/entities/Order.java
@@ -0,0 +1,72 @@
+package com.treino.registrodevendascomspringboot.entities;
+
+import java.io.Serializable;
+import java.time.Instant;
+
+import com.treino.registrodevendascomspringboot.entities.enums.OrderStatus;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@Entity
+@Table(name="tb_order")
+public class Order implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private Long id;
+	private Instant moment;
+	
+	@ManyToOne
+	@JoinColumn(name="client_id")
+	private User client;
+	
+	private Integer orderStatus;
+	public Order(Long id, Instant moment, User client, OrderStatus orderStatus) {
+		super();
+		this.id = id;
+		this.moment = moment;
+		this.client = client;
+		setOrderStatus(orderStatus);
+	}
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public Instant getMoment() {
+		return moment;
+	}
+	public void setMoment(Instant moment) {
+		this.moment = moment;
+	}
+	public User getclient() {
+		return client;
+	}
+	public void setclient(User client) {
+		this.client = client;
+	}
+	public OrderStatus getOrderStatus() {
+		return OrderStatus.valueOf(orderStatus);
+	}
+	public void setOrderStatus(OrderStatus orderStatus) {
+		if(orderStatus!=null) {
+			this.orderStatus=orderStatus.getCode();
+		}
+		
+	}
+	
+	
+	
+
+}
--- src/main/java/com/treino/registrodevendascomspringboot/entities/User.java
+++ src/main/java/com/treino/registrodevendascomspringboot/entities/User.java
@@ -1,10 +1,17 @@
 package com.treino.registrodevendascomspringboot.entities;
 
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -15,8 +22,10 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 @Table(name="tb_user")
-public class User {
+public class User implements Serializable{
 	
+	
+	private static final long serialVersionUID = 1L;
 	@Id
 	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private Long id;
@@ -28,5 +37,9 @@ public class User {
 	@Column(length = 10)
 	private String phone;
 	private String password;
+	
+	@JsonIgnore
+	@OneToMany(mappedBy="client")
+	private List<Order> order=new ArrayList<>();
 
 }
