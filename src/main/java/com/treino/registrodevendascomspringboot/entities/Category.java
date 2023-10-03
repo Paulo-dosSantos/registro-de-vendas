7a949014478461006033d2f849870218e4f9eb17
Paulo Henrique <paulo.13.henrique800@gmail.com>
2023-10-03 17:14:03 -0300

    criando entidade Category

---
+++ src/main/java/com/treino/registrodevendascomspringboot/entities/Category.java
@@ -0,0 +1,31 @@
+package com.treino.registrodevendascomspringboot.entities;
+
+import java.io.Serializable;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@Entity
+@Table(name="tb_category")
+@AllArgsConstructor
+
+public class Category implements Serializable {
+
+	
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private Long id;
+	
+	private String name;
+	
+	
+
+}
