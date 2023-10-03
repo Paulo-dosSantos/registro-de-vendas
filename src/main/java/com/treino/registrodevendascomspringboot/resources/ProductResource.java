29f940ba8faa1a365707a6fb3ee6473923b37c9f
Paulo Henrique <paulo.13.henrique800@gmail.com>
2023-10-03 20:20:34 -0300

    criando camada de recursos

---
+++ src/main/java/com/treino/registrodevendascomspringboot/resources/ProductResource.java
@@ -0,0 +1,37 @@
+package com.treino.registrodevendascomspringboot.resources;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.treino.registrodevendascomspringboot.entities.Category;
+import com.treino.registrodevendascomspringboot.entities.Product;
+import com.treino.registrodevendascomspringboot.services.ProductService;
+
+@RestController
+@RequestMapping(value="/categories")
+public class ProductResource {
+	
+	@Autowired
+	private ProductService service;
+	
+	
+	
+	@GetMapping
+	public ResponseEntity<List<Product>>findAll(){
+		List<Product>products=service.findAll();
+		
+		return ResponseEntity.ok().body(products);
+	}
+	@GetMapping(value="/{id}")
+	public ResponseEntity<Product>findById(@PathVariable Long id){
+		Product product= service.findById(id);
+		
+		return ResponseEntity.ok().body(product);
+	}
+}
