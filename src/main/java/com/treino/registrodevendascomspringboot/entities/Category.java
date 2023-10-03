95e79925a10453bd89337fb1c27051fb0d3da710
Paulo Henrique <paulo.13.henrique800@gmail.com>
2023-10-03 20:03:17 -0300

    criando entidade Product e relacionando-a com a entidade Category

--- src/main/java/com/treino/registrodevendascomspringboot/entities/Category.java
+++ src/main/java/com/treino/registrodevendascomspringboot/entities/Category.java
@@ -1,11 +1,16 @@
 package com.treino.registrodevendascomspringboot.entities;
 
 import java.io.Serializable;
+import java.util.HashSet;
+import java.util.Set;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -26,6 +31,10 @@ public class Category implements Serializable {
 	private Long id;
 	
 	private String name;
+	
+	@JsonIgnore
+	@ManyToMany(mappedBy="categories")
+	private Set<Product>products=new HashSet<>();
 	
 	
 
---
+++ src/main/java/com/treino/registrodevendascomspringboot/entities/Product.java
@@ -0,0 +1,57 @@
+package com.treino.registrodevendascomspringboot.entities;
+
+import java.io.Serializable;
+import java.util.HashSet;
+import java.util.Set;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name="tb_product")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Product implements Serializable{
+
+	
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private Long id;
+	private String name;
+	private String description;
+	private Double price;
+	private String imgUrl;
+	
+	@ManyToMany
+	@JoinTable(name="tb_product_category",joinColumns = 
+	@JoinColumn(name="product_id"),inverseJoinColumns = 
+	@JoinColumn(name="category_id"))
+	private Set<Category>categories= new HashSet<>();
+
+	public Product(Long id, String name, String description, Double price, String imgUrl) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.description = description;
+		this.price = price;
+		this.imgUrl = imgUrl;
+	}
+	
+	
+	
+	
+	
+
+}
