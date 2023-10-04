1155b8c954c53db926b2c8c017a0dc2746880a55
Paulo Henrique <paulo.13.henrique800@gmail.com>
2023-10-04 14:12:47 -0300

    criando testes para camada resource da categoria

--- src/main/java/com/treino/registrodevendascomspringboot/resources/CategoryResource.java
+++ src/main/java/com/treino/registrodevendascomspringboot/resources/CategoryResource.java
@@ -10,7 +10,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.treino.registrodevendascomspringboot.entities.Category;
-import com.treino.registrodevendascomspringboot.entities.Order;
 import com.treino.registrodevendascomspringboot.services.CategoryService;
 
 @RestController
---
+++ src/test/java/com/treino/registrodevendascomspringboot/resources/CategoryResourceTest.java
@@ -0,0 +1,90 @@
+package com.treino.registrodevendascomspringboot.resources;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.fail;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.Mockito.when;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.ResponseEntity;
+
+import com.treino.registrodevendascomspringboot.entities.Category;
+import com.treino.registrodevendascomspringboot.services.CategoryService;
+
+@SpringBootTest
+class CategoryResourceTest {
+	
+	@InjectMocks
+	private CategoryResource resource;
+	
+	@Mock
+	private CategoryService service;
+	
+	private Category category;
+	
+	private static final String NAME = "Books";
+
+	private static final long ID = 1L;
+
+	@BeforeAll
+	static void setUpBeforeClass() throws Exception {
+	}
+
+	@BeforeEach
+	void setUp() throws Exception {
+		MockitoAnnotations.openMocks(this);
+		startUser();
+	}
+
+	private void startUser() {
+		category=new Category(ID, NAME);
+		
+	}
+
+	@Test
+	void testFindAll() {
+		when(service.findAll()).thenReturn(List.of(category));
+		
+		ResponseEntity<List<Category>>response=resource.findAll();
+		
+		assertNotNull(response);
+		assertNotNull(response.getBody());
+		assertEquals(ResponseEntity.class,response.getClass());
+		
+		assertEquals(Category.class,response.getBody().get(0).getClass());
+		assertEquals(ID,response.getBody().get(0).getId());
+		assertEquals(NAME,response.getBody().get(0).getName());
+	}
+
+	@Test
+	void testFindById() {
+		when(service.findById(anyLong())).thenReturn(category);
+		
+		ResponseEntity<Category> response=resource.findById(ID);
+		
+		assertNotNull(response);
+		assertEquals(ResponseEntity.class,response.getClass());
+		assertEquals(Category.class,response.getBody().getClass());
+		assertEquals(ID,response.getBody().getId());
+		assertEquals(NAME,response.getBody().getName());
+	
+	
+	
+	
+	
+	
+	
+	
+	}
+
+}
